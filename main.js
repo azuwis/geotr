1b812d9da88f51aecab7f4c2b61e0a0e00dab69a
Zhong Jianxin <azuwis@gmail.com>
2017-02-26 14:53:55 +0800

    Add textarea

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-var points = [
-    {
-        address: '浙江, 杭州',
-    },
-    {
-        address: '上海',
+var clearSingle = function(obj) {
+    if (obj && $.isFunction(obj.setMap)) {
+        obj.setMap(null);
     }
-];
+}
 
-$(function() {
+var clearMap = function(obj) {
+    if (Array.isArray(obj)) {
+        obj.forEach(function(elem) { clearSingle(elem); });
+    } else {
+        clearSingle(obj);
+    }
+}
+
+var drawMap = function(points) {
     $('#map')
         .gmap3({
             zoom: 2,
             center: [0, -180]
+        })
+        .then(function(result){
+            clearMap(this.get(-1));
+            clearMap(this.get(-2));
         })
         .marker(points.map(function(elem, index) {
             return {
@@ -24,8 +33,22 @@ $(function() {
                 strokeColor: "#FF0000",
                 strokeOpacity: 1.0,
                 strokeWeight: 2,
-                path: markers.map(function(elem){ return elem.position; })
+                path: markers.map(function(elem) { return elem.position; })
             };
         })
         .fit();
+}
+
+$(function() {
+    $('#map')
+        .gmap3({
+            zoom: 2,
+            center: [0, -180]
+        });
+    $('#form').submit(function(event) {
+        var input = $(this).find('textarea').val();
+        var points = input.trim().split("\n").map(function(elem) { return {address: elem}; });
+        drawMap(points);
+        event.preventDefault();
+    });
 });
