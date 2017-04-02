650804ceba5faf1afa70c84f956711b6d0bbbf54
Zhong Jianxin <azuwis@gmail.com>
2017-04-02 21:03:07 +0800

    Let getInfo.ipapi use getInfoMulti

--- js/main.js
+++ js/main.js
@@ -71,55 +71,43 @@ $(function() {
 
     var getInfo = {
         ipapi: function(ips) {
-            var ipapiMapFunc = function(elem) {
-                var region = '', city = '', isp = '';
-                if (elem.isp.match(/(backbone|cnc group)/i)) {
-                    region = '';
-                    city = '';
-                } else {
-                    region = elem.regionName;
-                    city = elem.city;
-                }
-                isp = elem.isp;
-                if (elem.isp.match(/telecom/i)) {
-                    isp = 'TEL';
-                } else if (elem.isp.match(/(cnc|unicom)/i)) {
-                    isp = 'CNC';
-                }
-                return {
-                    country: elem.country,
-                    region: region,
-                    city: city,
-                    isp: isp,
-                    lat: elem.lat,
-                    lon: elem.lon
-                };
-            };
-            if (location.protocol == 'https:') {
-                return getInfoMulti('ipapi', ips, function(ip) {
+            var ipapiProxy = function(ip) {
+                if (location.protocol == 'https:') {
                     return $.yql({
                         url: 'http://ip-api.com/json/' + ip
-                    }).then(function(data) {
-                        return ipapiMapFunc(data);
                     });
-                });
-            } else {
-                return $.ajax({
-                    type: 'POST',
-                    url: '//ip-api.com/batch',
-                    data: JSON.stringify(ips.map(function(elem) {
-                        return {query: elem};
-                    })),
-                    dataType: 'json'
-                }).then(function(data) {
-                    return data.map(function(elem, index) {
-                        var info = ipapiMapFunc(elem);
-                        info.num = index + 1;
-                        info.ip  = elem.query;
-                        return info;
+                } else {
+                    return $.ajax({
+                        url: 'http://ip-api.com/json/' + ip
                     });
+                }
+            };
+            return getInfoMulti('ipapi', ips, function(ip) {
+                return ipapiProxy(ip).then(function(elem) {
+                    var region = '', city = '', isp = '';
+                    if (elem.isp.match(/(backbone|cnc group)/i)) {
+                        region = '';
+                        city = '';
+                    } else {
+                        region = elem.regionName;
+                        city = elem.city;
+                    }
+                    isp = elem.isp;
+                    if (elem.isp.match(/telecom/i)) {
+                        isp = 'TEL';
+                    } else if (elem.isp.match(/(cnc|unicom)/i)) {
+                        isp = 'CNC';
+                    }
+                    return {
+                        country: elem.country,
+                        region: region,
+                        city: city,
+                        isp: isp,
+                        lat: elem.lat,
+                        lon: elem.lon
+                    };
                 });
-            }
+            });
         },
         ipinfo: function(ips) {
             return getInfoMulti('ipinfo', ips, function(ip) {
